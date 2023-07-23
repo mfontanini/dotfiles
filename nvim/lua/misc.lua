f8db9f67a415a284bfb3375360e430b73fe02bb7
Matias Fontanini <matias.fontanini@gmail.com>
2023-07-23 12:30:00 -0700

    Use system clipboard

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -5,6 +5,8 @@ vim.wo.relativenumber = true
 vim.opt.colorcolumn = "120"
 vim.opt.guicursor = "i:block-blinkwait50-blinkon200-blinkoff150"
 
+vim.api.nvim_set_option("clipboard","unnamedplus")
+
 -- Always show diagnostics column
 vim.opt.signcolumn = "yes"
 
