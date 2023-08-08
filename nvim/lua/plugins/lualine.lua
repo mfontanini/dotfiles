714d61bb62d74534b69243f7cea7e0404ba50e9f
Matias Fontanini <matias.fontanini@gmail.com>
2023-08-08 13:23:42 -0700

    Add lualine progress bar

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,19 +1,26 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  commit = "05d78e9fd0cdfb4545974a5aa14b1be95a86e9c9", 
-  dependencies = {
-    "nvim-tree/nvim-web-devicons"
-  },
-  config = function()
-    require("lualine").setup {
-      sections = {
-        lualine_c = {
-          {
-            "filename",
-            path = 1,
+  {
+    "nvim-lualine/lualine.nvim",
+    commit = "05d78e9fd0cdfb4545974a5aa14b1be95a86e9c9", 
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      require("lualine").setup {
+        sections = {
+          lualine_c = {
+            {
+              "filename",
+              path = 1,
+            },
+            "lsp_progress",
           },
-        },
+        }
       }
-    }
-  end,
+    end,
+  },
+  {
+    "arkav/lualine-lsp-progress",
+    commit = "56842d097245a08d77912edf5f2a69ba29f275d7",
+  }
 }
