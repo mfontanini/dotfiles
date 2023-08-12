9c635a9a2ec1cb8402d980d64362bf58af07794e
Matias Fontanini <matias.fontanini@gmail.com>
2023-08-12 13:27:25 -0700

    Fix cursor highlighting

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -14,16 +14,7 @@ return {
         null_ls.builtins.formatting.black,
       },
       on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format()
-            end,
-          })
-        end
+        require("utils/lsp").enable_auto_formatting(client, bufnr)
       end,
     })
   end,
--- nvim/lua/utils/lsp.lua
+++ nvim/lua/utils/lsp.lua
@@ -1,21 +1,36 @@
 local M = {}
 
+local formatting_group = vim.api.nvim_create_augroup("lsp_formatting", { clear = true })
+local highlight_group = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+
 -- Highlight symbol under cursor. The delay is configured via `vim.opt.updatetime`.
 function M.enable_cursor_highlighting(client, bufnr)
   if client.server_capabilities.documentHighlightProvider then
-    vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-    vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+    vim.api.nvim_clear_autocmds { buffer = bufnr, group = highlight_group }
     vim.api.nvim_create_autocmd("CursorHold", {
         callback = vim.lsp.buf.document_highlight,
         buffer = bufnr,
-        group = "lsp_document_highlight",
+        group = highlight_group,
         desc = "Document highlight",
     })
     vim.api.nvim_create_autocmd("CursorMoved", {
         callback = vim.lsp.buf.clear_references,
         buffer = bufnr,
-        group = "lsp_document_highlight",
+        group = highlight_group,
         desc = "Clear all the references",
+    })
+  end
+end
+
+function M.enable_auto_formatting(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = formatting_group, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = formatting_group,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format()
+      end,
     })
   end
 end
