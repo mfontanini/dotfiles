9b6424c8762fb0d08d782dfb178cf7964ec7d034
Matias Fontanini <matias.fontanini@gmail.com>
2023-08-15 18:36:59 -0700

    Add some more common options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -8,12 +8,12 @@ vim.opt.shada = "!,'32,<50,s10,h"
 vim.opt.pumheight = 16
 vim.opt.colorcolumn = "120"
 vim.opt.guicursor = "i:block-blinkwait50-blinkon200-blinkoff150"
+vim.opt.undofile = true
+vim.opt.signcolumn = "yes"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
 
 vim.wo.relativenumber = true
 vim.wo.number = true
-
--- Always show diagnostics column
-vim.opt.signcolumn = "yes"
-
