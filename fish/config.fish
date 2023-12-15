9aa82ebb7e298955db84065fae6ef12f41d68da9
Matias Fontanini <matias.fontanini@gmail.com>
2023-12-14 17:06:22 -0800

    Use vim keybindings on fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
-bind \n down-line
-bind \v up-line
-
 if test -f ~/.config/fish/env.fish
   . ~/.config/fish/env.fish
 end
+
+bind -k backspace backward-kill-word
+bind \cF --mode insert accept-autosuggestion
+bind \n down-line
+bind \v up-line
