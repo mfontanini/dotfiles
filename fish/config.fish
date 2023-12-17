e8ef6d2ec30d054199d010866bd2308107319f90
Matias Fontanini <matias.fontanini@gmail.com>
2023-12-16 17:36:44 -0800

    Don't use vi keybindings on fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,5 @@ if test -f ~/.config/fish/env.fish
   . ~/.config/fish/env.fish
 end
 
-bind -k backspace backward-kill-word
-bind \cF --mode insert accept-autosuggestion
 bind \n down-line
 bind \v up-line
