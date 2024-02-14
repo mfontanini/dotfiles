c3b660d8d2b19ef9a6b1d8fa493cf211ccc68586
Matias Fontanini <matias.fontanini@gmail.com>
2024-02-14 14:16:13 -0800

    Add .gitconfig

--- .config/fish/functions/git-sync-master.fish
+++
@@ -1,4 +0,0 @@
-function git-sync-master --wraps='git checkout master && git pull origin master' --description 'alias git-sync-master=git checkout master && git pull origin master'
-  git checkout master && git pull origin master $argv
-        
-end
