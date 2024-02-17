560b74384cc5ceb63de8d4e5ce77ce386802afae
Matias Fontanini <matias.fontanini@gmail.com>
2024-02-17 10:05:56 -0800

    Don't install fonts if already installed

--- install.sh
+++ install.sh
@@ -2,14 +2,32 @@
 
 script_dir=$(dirname "$0")
 
+pushd "$script_dir" >/dev/null || exit
+
+fonts_installed() {
+	for font in fonts/*; do
+		if fc-list | grep "${font}" >/dev/null; then
+			return 0
+		fi
+	done
+	return 1
+}
+
 mkdir -p ~/.config
 
-pushd $script_dir
-stow --target="$HOME" -v .
+echo "stowing..."
 
-if [ $? -ne 0 ]; then
+if stow --target="$HOME" -v .; then
+	echo "stow done"
+else
 	echo "stow failed!"
 	exit 1
 fi
 
-fc-cache -fv
+if fonts_installed; then
+	echo "fonts already installed"
+else
+	echo "installing fonts..."
+	fc-cache -f
+	echo "fonts installed"
+fi
