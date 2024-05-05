0398d69f92ec486f8a2df37ca388019e05e90bf3
Matias Fontanini <matias.fontanini@gmail.com>
2024-05-05 10:04:07 -0700

    Allow auto updating versions

--- check-for-updates.sh
+++ check-for-updates.sh
@@ -3,8 +3,6 @@
 set -e
 
 script_dir=$(dirname "$0")
-source "${script_dir}/versions.sh"
-source "${script_dir}/logging.sh"
 
 normalize_version() {
   if [[ "$1" == v* ]]; then
@@ -14,23 +12,74 @@ normalize_version() {
   fi
 }
 
-check_version() {
-  repo=$1
-  version=$(normalize_version $2)
+curl_github() {
+  curl "$1" \
+    --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+    --header "X-GitHub-Api-Version: 2022-11-28" \
+    -s
+}
 
-  latest=$(curl "https://api.github.com/repos/${repo}/releases/latest" -s | jq -r .tag_name)
+check_version() {
+  version_var=$1
+  repo=$2
+  version=$(normalize_version $3)
+  auto_update=$4
+
+  latest=$(curl_github "https://api.github.com/repos/${repo}/releases/latest" | jq -r .tag_name)
   normalized_latest=$(normalize_version "$latest")
   if [ "$version" != "$normalized_latest" ]; then
     release_url="http://github.com/${repo}/releases/${latest}"
     warn "${repo} update available: we're using ${version} but latest is ${latest}: ${release_url}"
+    if [ "${auto_update}" == "1" ]; then
+      info "updating version in versions.sh for ${dependency} to ${normalized_latest}"
+      sed -i "s/^${version_var}=.*$/${version_var}=${normalized_latest}/g" "${script_dir}/versions.sh"
+    fi
+    return 0
   else
     success "${repo} is up to date"
+    return 1
   fi
 }
 
-dependencies=(nvim fzf alacritty mold delta tmux pyright)
-for dependency in "${dependencies[@]}"; do
-  repo_var="${dependency^^}_REPO"
-  version_var="${dependency^^}_VERSION"
-  check_version "${!repo_var}" "${!version_var}"
-done
+print_usage() {
+  echo "Usage: $0 [-a]"
+  exit 1
+}
+
+main() {
+  source "${script_dir}/versions.sh"
+  source "${script_dir}/logging.sh"
+  source "${script_dir}/.env"
+
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+    -h | --help)
+      print_usage $0
+      ;;
+    -a | --auto-update)
+      auto_update=1
+      shift
+      ;;
+    -* | --*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    esac
+  done
+
+  any_updates=0
+  dependencies=(nvim fzf alacritty mold delta tmux pyright)
+  for dependency in "${dependencies[@]}"; do
+    repo_var="${dependency^^}_REPO"
+    version_var="${dependency^^}_VERSION"
+    if check_version "${version_var}" "${!repo_var}" "${!version_var}" "${auto_update}"; then
+      any_updates=1
+    fi
+  done
+  if [ "$any_updates" -eq 1 ] && [ "$auto_update" == "1" ]; then
+    warn "updates found, running install script"
+    "${script_dir}/install.sh"
+  fi
+}
+
+main $@
