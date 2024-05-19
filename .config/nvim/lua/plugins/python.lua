6dc923837a88840f1afdc698d00703e69b4e1afd
Matias Fontanini <matias.fontanini@gmail.com>
2024-05-19 07:53:21 -0700

    Fix pyright setup

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -1,9 +1,5 @@
 local lspconfig = require("lspconfig")
 lspconfig.ruff_lsp.setup {}
-lspconfig.pyright.setup {
-  on_attach = function(client, bufnr)
-    vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-  end,
-}
+lspconfig.pyright.setup {}
 
 return {}
