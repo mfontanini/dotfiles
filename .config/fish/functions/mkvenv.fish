f58de2313e58c8b1a200b6098ad661022b7111bc
Matias Fontanini <matias.fontanini@gmail.com>
2024-10-10 06:33:00 -0700

    Fix `test` call in `mkvenv`

--- .config/fish/functions/mkvenv.fish
+++ .config/fish/functions/mkvenv.fish
@@ -1,7 +1,7 @@
 function mkvenv --description "Make a python venv and activate it"
   uv venv
   source .venv/bin/activate.fish
-  if test requirements.txt
+  if test -f requirements.txt
     uv pip install -r requirements.txt
   end
 end
