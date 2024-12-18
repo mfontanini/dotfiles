36092e032dcbf3efdfa0476baa2fb1a96cd3bdba
Matias Fontanini <matias.fontanini@gmail.com>
2024-12-18 09:53:54 -0800

    Allow searching within node in neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,3 +1,25 @@
+local function getTelescopeOpts(state, path)
+  return {
+    cwd = path,
+    search_dirs = { path },
+    attach_mappings = function (prompt_bufnr, map)
+      local actions = require "telescope.actions"
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local action_state = require "telescope.actions.state"
+        local selection = action_state.get_selected_entry()
+        local filename = selection.filename
+        if (filename == nil) then
+          filename = selection[1]
+        end
+        -- any way to open the file without triggering auto-close event of neo-tree?
+        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+      end)
+      return true
+    end
+  }
+end
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
@@ -16,6 +38,11 @@ return {
         filtered_items = {
           hide_dotfiles = false,
         },
+        window = {
+          mappings = {
+            ["<c-f>"] = "telescope_grep",
+          },
+        },
       },
       event_handlers = {
         {
@@ -29,6 +56,13 @@ return {
         type = {
           enabled = false,
         },
+      },
+      commands = {
+        telescope_grep = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          require('telescope.builtin').live_grep(getTelescopeOpts(state, path))
+        end,
       },
     })
   end
