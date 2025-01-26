c0664318929de618f4a7c51521b2cd8d84b3fb8b
Matias Fontanini <matias.fontanini@gmail.com>
2025-01-26 15:19:13 -0800

    Moar `success`

--- install.sh
+++ install.sh
@@ -40,66 +40,66 @@ install_tar_binary() {
 
 install_neovim() {
   if nvim --version 2>/dev/null | head -n 1 | grep "^NVIM v${NVIM_VERSION}$" >/dev/null; then
-    info neovim is up to date
+    success neovim is up to date
   else
     warn installing neovim ${NVIM_VERSION}...
     install_binary \
       "https://github.com/${NVIM_REPO}/releases/download/v${NVIM_VERSION}/nvim.appimage" \
       nvim
-    info neovim installed
+    success neovim installed
   fi
 }
 
 install_fzf() {
   if fzf --version 2>/dev/null | grep "^${FZF_VERSION} " >/dev/null; then
-    info fzf is up to date
+    success fzf is up to date
   else
     warn installing fzf ${FZF_VERSION}...
     install_tar_binary "https://github.com/${FZF_REPO}/releases/download/v${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz" \
       fzf \
       fzf
-    info fzf installed
+    success fzf installed
   fi
 }
 
 install_gh() {
   if gh --version 2>/dev/null | grep "^gh version ${GH_VERSION} " >/dev/null; then
-    info gh is up to date
+    success gh is up to date
   else
     warn installing gh ${GH_VERSION}...
     install_tar_binary \
       "https://github.com/${GH_REPO}/releases/download/v${GH_VERSION}/gh_2.65.0_linux_amd64.tar.gz" \
       "gh_${GH_VERSION}_linux_amd64/bin/gh" \
       gh
-    info gh installed
+    success gh installed
   fi
 }
 
 install_jq() {
   if jq -V 2>/dev/null | grep "^${JQ_VERSION}$" >/dev/null; then
-    info jq is up to date
+    success jq is up to date
   else
     warn installing jq ${JQ_VERSION}...
     install_binary \
       "https://github.com/${JQ_REPO}/releases/download/${JQ_VERSION}/jq-linux64" \
       jq
-    info jq installed
+    success jq installed
   fi
 }
 
 install_alacritty() {
   if alacritty -V 2>/dev/null | grep "^alacritty ${ALACRITTY_VERSION}$" >/dev/null; then
-    info alacritty is up to date
+    success alacritty is up to date
   else
     warn installing alacritty ${ALACRITTY_VERSION}...
     cargo install -q alacritty
-    info alacritty installed
+    success alacritty installed
   fi
 }
 
 install_mold() {
   if mold --version 2>/dev/null | grep "^mold ${MOLD_VERSION} " >/dev/null; then
-    info mold is up to date
+    success mold is up to date
   else
     warn installing mold ${MOLD_VERSION}...
     dir="mold-${MOLD_VERSION}-x86_64-linux"
@@ -110,60 +110,55 @@ install_mold() {
     tar xvzf "${filename}" >/dev/null
     cp -r ${dir}/* ~/.local
     popd >/dev/null
-    info mold installed
+    success mold installed
   fi
 }
 
 install_delta() {
   if delta -V 2>/dev/null | grep "^delta ${DELTA_VERSION}$" >/dev/null; then
-    info delta is up to date
+    success delta is up to date
   else
     warn installing delta ${DELTA_VERSION}...
     install_tar_binary "https://github.com/${DELTA_REPO}/releases/download/${DELTA_VERSION}/delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu.tar.gz" \
       "delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu/delta" \
       delta
-    info delta installed
+    success delta installed
   fi
 }
 
 install_uv() {
   if uv --version 2>/dev/null | grep "^uv ${UV_VERSION}$" >/dev/null; then
-    info uv is up to date
+    success uv is up to date
   else
     warn installing uv ${UV_VERSION}...
     curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/${UV_VERSION}/uv-installer.sh | sh
-    info uv installed
-    if ! test -d "$VENV_PATH"; then
-      warn devenv pyenv does not exist, creating
-      uv venv "$VENV_PATH"
-      info pyenv created at "$VENV_PATH"
-    fi
+    success uv installed
   fi
 }
 
 install_pyright() {
   if "${VENV_PATH}/bin/pyright" --version 2>/dev/null | grep "^pyright ${PYRIGHT_VERSION}$" >/dev/null; then
-    info pyright is up to date
+    success pyright is up to date
   else
     warn installing pyright ${PYRIGHT_VERSION}...
     uv pip install --upgrade pyright
-    info pyright installed
+    success pyright installed
   fi
 }
 
 install_ruff() {
   if "${VENV_PATH}/bin/ruff-lsp" --version 2>/dev/null | grep "^ruff-lsp ${RUFF_VERSION}$" >/dev/null; then
-    info ruff is up to date
+    success ruff is up to date
   else
     warn installing ruff ${RUFF_VERSION}...
     uv pip install --upgrade ruff-lsp
-    info ruff-lsp installed
+    success ruff-lsp installed
   fi
 }
 
 install_tmux() {
   if tmux -V 2>/dev/null | grep "^tmux ${TMUX_VERSION}$" >/dev/null; then
-    info tmux is up to date
+    success tmux is up to date
   else
     warn installing tmux ${TMUX_VERSION}...
     dir="tmux-${TMUX_VERSION}"
@@ -178,20 +173,20 @@ install_tmux() {
     MAKEFLAGS=--silent make
     MAKEFLAGS=--silent make install
     popd >/dev/null
-    info tmux installed
+    success tmux installed
   fi
 }
 
 install_fish() {
   if which fish >/dev/null; then
-    info fish is up to date
+    success fish is up to date
   else
     warn installing fish shell...
     sudo apt-add-repository -y ppa:fish-shell/release-3 >/dev/null
     sudo apt -qq update
     sudo apt -qq install -y fish >/dev/null
     sudo chsh -s /usr/local/bin/fish >/dev/null
-    info fish shell installed
+    success fish shell installed
   fi
 }
 
@@ -207,21 +202,21 @@ install_cli_tools() {
   install_delta
   install_uv
   install_fish
-  info all cli tools installed
+  success all cli tools installed
 }
 
 install_python_tools() {
   info installing python tools
   install_pyright
   install_ruff
-  info all python tools installed
+  success all python tools installed
 }
 
 install_gui_tools() {
   info installing gui tools
   install_fonts
   install_alacritty
-  info all gui tools installed
+  success all gui tools installed
 }
 
 symlink_dotfiles() {
@@ -270,7 +265,7 @@ apt_install_core_tools() {
     sudo apt -qq update
     sudo apt -qq install -y ${missing[@]} >/dev/null
   else
-    info all core tools are installed
+    success all core tools are installed
   fi
 }
 
