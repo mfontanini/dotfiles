e824b57762bad9aaa756b99a13ac6b9117fb6b33
Matias Fontanini <matias.fontanini@gmail.com>
2025-03-07 16:12:56 -0800

    Bump

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -1,6 +1,6 @@
 local lspconfig = require("lspconfig")
-lspconfig.ruff_lsp.setup {
-  cmd = { "/home/matias/.cache/devenv/venv/bin/ruff-lsp" },
+lspconfig.ruff.setup {
+  cmd = { "/home/matias/.cache/devenv/venv/bin/ruff", "server" },
 }
 lspconfig.pyright.setup {
   cmd = { "/home/matias/.cache/devenv/venv/bin/pyright-langserver", "--stdio" },
